0b3b237bbfeb801739b289c016ee9cb60950c1ea
jdeveloperanalyst <jonatas1200@icloud.com>
2022-09-29 19:06:01 -0300

    teste

--- AnáliseDeDados.py
+++ AnáliseDeDados.py
@@ -4,25 +4,25 @@ import pyperclip
 import os
 import shutil
 
-#abre o navegador
-pyautogui.PAUSE = 1
-pyautogui.press('winleft')
-
-pyautogui.write('chrome')
-pyautogui.press('enter')
-pyautogui.hotkey('ctrl', 't')
+# #abre o navegador
+# pyautogui.PAUSE = 1
+# pyautogui.press('winleft')
+#
+# pyautogui.write('chrome')
+# pyautogui.press('enter')
+# pyautogui.hotkey('ctrl', 't')
 
 #abre o drive
-link = 'https://drive.google.com/drive/folders/1KlpEd8rfJiWP237HW8zEhobL44vBE3DA'
-pyperclip.copy(link)
-pyautogui.hotkey('ctrl', 'v')
-pyautogui.press('enter')
-time.sleep(7)
+# link = 'https://drive.google.com/drive/folders/1KlpEd8rfJiWP237HW8zEhobL44vBE3DA'
+# pyperclip.copy(link)
+# pyautogui.hotkey('ctrl', 'v')
+# pyautogui.press('enter')
+# time.sleep(7)
 
 #faz download da base de dados
-pyautogui.click(x=387, y=382, button='right')
-pyautogui.click(x=530, y=844)
-time.sleep(10)
+# pyautogui.click(x=387, y=382, button='right')
+# pyautogui.click(x=530, y=844)
+# time.sleep(10)
 
 #Coleta ultimo arquivo que foi feito download (pasta de origem)
 origem = r'C:\Users\jonat\Downloads'
@@ -38,19 +38,30 @@ print(ultimo_arq[1])  #segundo índice sobre a linha coletada na lista de arquiv
 
 #Mostra todos os arquivos sobre a pasta historico base de dados (pasta destino)
 lista2 = os.listdir(destino)
-list_arq2 = []
-for arq in lista2:
-    data2 = os.path.getctime(f"{destino}/{arq}")
-    list_arq2.append((data2, arq))
-list_arq2.sort(reverse=True)
-print(list_arq2)
+print(lista2)
 
 #move arquivo para outra pasta para manter um backup/log
-try:
-    if 'Vendas - Dez.xlsx' not in list_arq2 and 'Vendas - Dez - Copy.xlsx' not in list_arq2:
-        new_path = shutil.move(f"{origem}/{ultimo_arq[1]}", destino)
-except:
-    print('Arquivo já existe')
-    os.rename(r'C:\Users\jonat\Downloads\Vendas - Dez.xlsx', r'C:\Users\jonat\Documents\Meus Projetos\Python\Projeto 1 Automação de Análise de Dados\Analise-de-Dados\Histórico Base de Dados/Vendas - Dez - Copy.xlsx')
-    print('Arquivo renomeado e movido para pasta de  Destino')
-print('teste')
+if 'Vendas - Dez.xlsx' not in lista2:
+    shutil.move(f"{origem}/{ultimo_arq[1]}", destino)
+elif 'Vendas - Dez.xlsx' in lista2:
+    os.rename(r'C:\Users\jonat\Downloads\Vendas - Dez.xlsx', r'C:\Users\jonat\Downloads\Vendas - Dez - Copy.xlsx')
+    time.sleep(5)
+    shutil.move(f"{origem}/{ultimo_arq[1]}", destino)
+
+
+
+
+
+# try:
+#     if 'Vendas - Dez.xlsx' not in list_arq2:
+#         new_path = shutil.move(f"{origem}/{ultimo_arq[1]}", destino)
+# except:
+#     print('Arquivo já existe')
+#     cont = 0
+#     for line in list_arq2:
+#         if 'Vendas - Dez.xlsx' in line[1]:
+#             os.rename(r'C:\Users\jonat\Downloads\Vendas - Dez.xlsx', r'C:\Users\jonat\Documents\Meus Projetos\Python\Projeto 1 Automação de Análise de Dados\Analise-de-Dados\Histórico Base de Dados/Vendas - Dez - Copy.xlsx')
+#         elif 'Vendas - Dez - Copy.xlsx' in line[1]:
+#             cont += 1
+#             os.rename(r'C:\Users\jonat\Downloads\Vendas - Dez.xlsx', fr'C:\Users\jonat\Documents\Meus Projetos\Python\Projeto 1 Automação de Análise de Dados\Analise-de-Dados\Histórico Base de Dados/Vendas - Dez - Copy{cont}.xlsx')
+#     print('Arquivo renomeado e movido para pasta de  Destino')
